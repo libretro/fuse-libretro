d89777b912c760cf02151bc4736c4e810ae07af8
Andre Leiradella <andre@leiradella.com>
2016-07-17 22:01:23 +0100

    removed comments and marked parameter as unused

--- src/compat/timer.c
+++ src/compat/timer.c
@@ -10,12 +10,5 @@ double compat_timer_get_time( void )
 
 void compat_timer_sleep(int ms)
 {
-   // retro_time_t t0 = perf_cb.get_time_usec();
-   // retro_time_t t1;
-  
-   // /* Busy wait! */
-   // do {
-     // t1 = perf_cb.get_time_usec();
-   // }
-   // while ( ( t1 - t0 ) / 1000 < ms );
+  (void)ms;
 }
