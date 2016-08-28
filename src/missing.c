6fe9c48170a29ad9f148d6ae72644bed2e4af63e
Andre Leiradella <andre@leiradella.com>
2016-08-28 19:51:32 +0100

    added missing functions to ps vita builds

---
+++ src/missing.c
@@ -0,0 +1,22 @@
+#ifdef VITA
+
+#include <stddef.h>
+
+char* getcwd( char* buf, size_t size )
+{
+  if ( buf != NULL && size >= 2 )
+  {
+    buf[ 0 ] = '.';
+    buf[ 1 ] = 0;
+    return buf;
+  }
+
+  return NULL;
+}
+
+int chdir( const char* path)
+{
+  return 0;
+}
+
+#endif
