bc92f852dd6ddf20813e7d33680d404665359863
Francisco José García García <frangarcj@gmail.com>
2016-09-07 01:16:47 +0200

    (VITA) Fix stat

--- fuse/settings.c
+++ fuse/settings.c
@@ -2741,6 +2741,9 @@ read_config_file( settings_info *settings )
 
   /* See if the file exists; if doesn't, it's not a problem */
   if( stat( path, &stat_info ) ) {
+#if defined(VITA)
+    return 0;
+#else
     if( errno == ENOENT ) {
       return 0;
     } else {
@@ -2748,6 +2751,7 @@ read_config_file( settings_info *settings )
 		strerror( errno ) );
       return 1;
     }
+#endif
   }
 
   error = utils_read_file( path, &file );
