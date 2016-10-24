35d28b10d9499e2ed23a982a761075acb7ddb237
Andre Leiradella <andre@leiradella.com>
2016-10-24 19:00:49 +0100

    Fixed snapshot memory management.

--- src/fuse/snapshot.c
+++ src/fuse/snapshot.c
@@ -14,18 +14,29 @@ libspectrum_error fuse_libspectrum_snap_write( libspectrum_byte **buffer, size_t
 // This is an ugly hack so that we can have the frontend manage snapshots for us
 int fuse_write_snapshot(const char *filename, const unsigned char *buffer, size_t length)
 {
+   void* new_buffer;
+
    log_cb(RETRO_LOG_DEBUG, "%s(\"%s\", %p, %lu)\n", __FUNCTION__, filename, buffer, length);
-   
-   snapshot_buffer = malloc(length);
-   
-   if (snapshot_buffer)
+
+   if (snapshot_size < length)
    {
-      memcpy(snapshot_buffer, buffer, length);
+      new_buffer = realloc(snapshot_buffer, length);
+
+      if (!new_buffer)
+      {
+         free(snapshot_buffer);
+         snapshot_buffer = NULL;
+         snapshot_size = 0;
+
+         return 1;
+      }
+
+      snapshot_buffer = new_buffer;
       snapshot_size = length;
-      return 0;
    }
-   
-   return 1;
+
+   memcpy(snapshot_buffer, buffer, length);
+   return 0;
 }
 
 libspectrum_error fuse_libspectrum_snap_write( libspectrum_byte **buffer, size_t *length, int *out_flags, libspectrum_snap *snap, libspectrum_id_t type, libspectrum_creator *creator, int in_flags )
--- src/libretro.c
+++ src/libretro.c
@@ -252,49 +252,49 @@ static const struct retro_variable core_vars[] =
    { "fuse_key_hold_time", "Time to Release Key in ms; 500|1000|100|300" },
    { NULL, NULL },
 };
- 
+
 int update_variables(int force)
 {
    int flags = 0;
-   
+
    if (force)
    {
       // Only change the machine when reloading content
       int option = coreopt(env_cb, core_vars, "fuse_machine", NULL);
       option += option < 0;
       const machine_t *new_machine = machine_list + option;
-      
+
       if (new_machine != machine || force)
       {
          if (settings_current.start_machine)
          {
             libspectrum_free((void*)settings_current.start_machine);
          }
-         
+
          settings_current.start_machine = utils_safe_strdup(new_machine->fuse_id);
-         
+
          if (machine == NULL || new_machine->id == LIBSPECTRUM_MACHINE_48_NTSC || machine->id == LIBSPECTRUM_MACHINE_48_NTSC)
          {
             // region and fps change
             flags |= UPDATE_AV_INFO;
          }
-         
+
          machine = new_machine;
          frame_time = 1000.0 / ( machine->id == LIBSPECTRUM_MACHINE_48_NTSC ? 60.0 : 50.0 );
          flags |= UPDATE_MACHINE;
       }
-      
+
       unsigned width = machine->is_timex ? 640 : 320;
       unsigned height = machine->is_timex ? 480 : 240;
-      
+
       if (width != hard_width || height != hard_height || force)
       {
          hard_width = width;
          hard_height = height;
-         
+
          hide_border = coreopt(env_cb, core_vars, "fuse_hide_border", NULL);
          hide_border += hide_border < 0;
-         
+
          if (hide_border)
          {
             soft_width = machine->is_timex ? 512 : 256;
@@ -305,7 +305,7 @@ int update_variables(int force)
             soft_width = hard_width;
             soft_height = hard_height;
          }
-         
+
          first_pixel = (hard_height - soft_height) / 2 * hard_width + (hard_width - soft_width) / 2;
          flags |= UPDATE_AV_INFO | UPDATE_GEOMETRY;
       }
@@ -315,11 +315,11 @@ int update_variables(int force)
       // When reloading content, this is already done as part of the machine change
       int option = coreopt(env_cb, core_vars, "fuse_hide_border", NULL);
       option += option < 0;
-      
+
       if (option != hide_border || force)
       {
          hide_border = option;
-         
+
          if (hide_border)
          {
             soft_width = machine->is_timex ? 512 : 256;
@@ -330,12 +330,12 @@ int update_variables(int force)
             soft_width = hard_width;
             soft_height = hard_height;
          }
-         
+
          first_pixel = (hard_height - soft_height) / 2 * hard_width + (hard_width - soft_width) / 2;
          flags |= UPDATE_GEOMETRY;
       }
    }
-   
+
    settings_current.fastload = coreopt(env_cb, core_vars, "fuse_fast_load", NULL) != 1;
    settings_current.accelerate_loader = settings_current.fastload;
 
@@ -343,23 +343,23 @@ int update_variables(int force)
 
    {
       int option = coreopt(env_cb, core_vars, "fuse_speaker_type", NULL);
-      
+
       if (settings_current.speaker_type)
       {
          libspectrum_free((void*)settings_current.speaker_type);
       }
-      
+
       settings_current.speaker_type = utils_safe_strdup(option == 1 ? "Beeper" : option == 2 ? "Unfiltered" : "TV speaker");
    }
 
    {
       int option = coreopt(env_cb, core_vars, "fuse_ay_stereo_separation", NULL);
-      
+
       if (settings_current.stereo_ay)
       {
          libspectrum_free((void*)settings_current.stereo_ay);
       }
-      
+
       settings_current.stereo_ay = utils_safe_strdup(option == 1 ? "ACB" : option == 2 ? "ABC" : "None");
    }
 
@@ -370,7 +370,7 @@ int update_variables(int force)
       int option = coreopt(env_cb, core_vars, "fuse_key_hold_time", &value);
       keyb_hold_time = option >= 0 ? strtoll(value, NULL, 10) * 1000LL : 500000LL;
    }
-   
+
    return flags;
 }
 
@@ -386,7 +386,7 @@ static int get_joystick(unsigned device)
       case RETRO_DEVICE_TIMEX2_JOYSTICK:    return 6;
       case RETRO_DEVICE_FULLER_JOYSTICK:    return 7;
    }
-   
+
    return 0;
 }
 
@@ -412,11 +412,11 @@ void retro_set_environment(retro_environment_t cb)
       { "Timex 2 Joystick", RETRO_DEVICE_TIMEX2_JOYSTICK },
       { "Fuller Joystick", RETRO_DEVICE_FULLER_JOYSTICK }
    };
-   
+
    static const struct retro_controller_description keyboards[] = {
       { "Sinclair Keyboard", RETRO_DEVICE_SPECTRUM_KEYBOARD },
    };
-   
+
    static const struct retro_controller_info ports[MAX_PADS + 1] = {
       { controllers, sizeof(controllers) / sizeof(controllers[0]) }, // port 1
       { controllers, sizeof(controllers) / sizeof(controllers[0]) }, // port 2
@@ -428,7 +428,7 @@ void retro_set_environment(retro_environment_t cb)
    // even when we load a game via the frontend after booting to BASIC.
    //bool yes = true;
    //cb(RETRO_ENVIRONMENT_SET_SUPPORT_NO_GAME, (void*)&yes);
-   
+
    cb(RETRO_ENVIRONMENT_SET_VARIABLES, (void*)core_vars);
    cb(RETRO_ENVIRONMENT_SET_CONTROLLER_INFO, (void*)ports);
 }
@@ -446,7 +446,7 @@ void retro_init(void)
    {
       log_cb = log.log;
    }
-   
+
    machine = machine_list;
    total_time_ms = 0.0;
 
@@ -460,21 +460,21 @@ static libspectrum_id_t identify_file(const void* data, size_t size)
 {
    libspectrum_id_t type;
    libspectrum_error error = libspectrum_identify_file(&type, NULL, (const unsigned char*)data, size);
-   
+
    if (type != LIBSPECTRUM_ID_UNKNOWN)
    {
       return type;
    }
-   
+
    libspectrum_snap* snap = libspectrum_snap_alloc();
    error = libspectrum_snap_read(snap, (const libspectrum_byte*)data, size, LIBSPECTRUM_ID_SNAPSHOT_Z80, NULL);
    libspectrum_snap_free(snap);
-   
+
    if (error == LIBSPECTRUM_ERROR_NONE)
    {
       return LIBSPECTRUM_ID_SNAPSHOT_Z80;
    }
-   
+
    // Default to TRD, we won't be able to load TRD files otherwise
    return LIBSPECTRUM_ID_DISK_TRD;
 }
@@ -482,7 +482,7 @@ static libspectrum_id_t identify_file(const void* data, size_t size)
 static libspectrum_id_t indentify_file_get_ext(const void* data, size_t size, const char** ext)
 {
    libspectrum_id_t type = identify_file(data, size);
-   
+
    switch (type)
    {
       case LIBSPECTRUM_ID_RECORDING_RZX: *ext = ".rzx"; break;
@@ -506,7 +506,7 @@ static libspectrum_id_t indentify_file_get_ext(const void* data, size_t size, co
       case LIBSPECTRUM_ID_DISK_TRD:      *ext = ".trd"; break;
       default:                           *ext = "";     break;
    }
-   
+
    return type;
 }
 
@@ -515,7 +515,7 @@ extern const char* fuse_gitstamp;
 bool retro_load_game(const struct retro_game_info *info)
 {
    log_cb( RETRO_LOG_INFO, "\n%s", fuse_gitstamp );
-   
+
    enum retro_pixel_format fmt = RETRO_PIXEL_FORMAT_RGB565;
 
    if (!env_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &fmt))
@@ -523,7 +523,7 @@ bool retro_load_game(const struct retro_game_info *info)
       log_cb(RETRO_LOG_ERROR, "RGB565 is not supported\n");
       return false;
    }
-   
+
    env_cb(RETRO_ENVIRONMENT_SET_INPUT_DESCRIPTORS, input_descriptors);
    memset(joypad_state, 0, sizeof(joypad_state));
    memset(keyb_state, 0, sizeof(keyb_state));
@@ -532,34 +532,35 @@ bool retro_load_game(const struct retro_game_info *info)
    keyb_x = keyb_y = 0;
    keyb_send = 0;
    snapshot_buffer = NULL;
-   
+   snapshot_size = 0;
+
    char *argv[] = {
       "fuse",
    };
-   
+
    if (fuse_init(sizeof(argv) / sizeof(argv[0]), argv) == 0)
    {
       if (info->size != 0)
       {
          tape_size = info->size;
          tape_data = malloc(tape_size);
-         
+
          if (!tape_data)
          {
             log_cb(RETRO_LOG_ERROR, "Could not allocate memory for the tape");
             fuse_end();
             return false;
          }
-         
+
          memcpy(tape_data, info->data, tape_size);
-         
+
          const char* ext;
          libspectrum_id_t type = indentify_file_get_ext(tape_data, tape_size, &ext);
-         
+
          char filename[32];
          snprintf(filename, sizeof(filename), "*%s", ext);
          filename[sizeof(filename) - 1] = 0;
-         
+
          fuse_emulation_pause();
          utils_open_file(filename, 1, &type);
          display_refresh_all();
@@ -571,10 +572,10 @@ bool retro_load_game(const struct retro_game_info *info)
          tape_data = NULL;
          tape_size = 0;
       }
-   
+
       // Enable read/write on all disk drives
       int i;
-      
+
       for (i = 0; i < 2; i++)
       {
          specplus3_disk_writeprotect( i, 0 );
@@ -582,20 +583,20 @@ bool retro_load_game(const struct retro_game_info *info)
          opus_disk_writeprotect( i, 0 );
          disciple_disk_writeprotect( i, 0 );
       }
-      
+
       for (i = 0; i < 4; i++)
       {
          beta_disk_writeprotect( i, 0 );
       }
-      
+
       for (i = 0; i < 8; i++)
       {
          if1_mdr_writeprotect( i, 0 );
       }
-      
+
       return true;
    }
-   
+
    return false;
 }
 
@@ -640,7 +641,7 @@ void retro_get_system_av_info(struct retro_system_av_info *info)
    // without the video border
    info->geometry.base_width = hard_width;
    info->geometry.base_height = hard_height;
-   
+
    info->geometry.max_width = MAX_WIDTH;
    info->geometry.max_height = MAX_HEIGHT;
    info->geometry.aspect_ratio = 0.0f;
@@ -664,7 +665,7 @@ static void render_video(void)
             const uint16_t* src2 = image_buffer;
             uint16_t* dest = image_buffer_2;
             int x, y;
-            
+
             if (keyb_transparent)
             {
                for (y = 0; y < 240; y++)
@@ -672,16 +673,16 @@ static void render_video(void)
                   for (x = 0; x < 320; x++)
                   {
                      uint32_t src1_pixel = (*src1++ & 0xe79c) * 3;
-                     
+
                      dest[ 0 ] = (src1_pixel + ( src2[ 0 ] & 0xe79c)) >> 2;
                      dest[ 1 ] = (src1_pixel + ( src2[ 1 ] & 0xe79c)) >> 2;
                      dest[ 640 ] = (src1_pixel + ( src2[ 640 ] & 0xe79c)) >> 2;
                      dest[ 641 ] = (src1_pixel + ( src2[ 641 ] & 0xe79c)) >> 2;
-                     
+
                      src2 += 2;
                      dest += 2;
                   }
-                  
+
                   src2 += 640;
                   dest += 640;
                }
@@ -693,16 +694,16 @@ static void render_video(void)
                   for (x = 0; x < 320; x++)
                   {
                      uint32_t src1_pixel = *src1++;
-                     
+
                      dest[ 0 ] = src1_pixel;
                      dest[ 1 ] = src1_pixel;
                      dest[ 640 ] = src1_pixel;
                      dest[ 641 ] = src1_pixel;
-                     
+
                      src2 += 2;
                      dest += 2;
                   }
-                  
+
                   src2 += 640;
                   dest += 640;
                }
@@ -716,12 +717,12 @@ static void render_video(void)
                const uint16_t* src2 = image_buffer;
                const uint16_t* end = src1 + sizeof(keyboard_overlay) / sizeof(keyboard_overlay[0]);
                uint16_t* dest = image_buffer_2;
-            
+
                do
                {
                   uint32_t src1_pixel = *src1++ & 0xe79c;
                   uint32_t src2_pixel = *src2++ & 0xe79c;
-                  
+
                   *dest++ = (src1_pixel * 3 + src2_pixel) >> 2;
                }
                while (src1 < end);
@@ -731,11 +732,11 @@ static void render_video(void)
                memcpy(image_buffer_2, keyboard_overlay, sizeof(keyboard_overlay));
             }
          }
-         
+
          unsigned x = keyb_positions[keyb_y].x + keyb_x * 24;
          unsigned y = keyb_positions[keyb_y].y;
          unsigned width = 23;
-         
+
          if (keyb_y == 3)
          {
             if (keyb_x == 8)
@@ -748,11 +749,11 @@ static void render_video(void)
                width = 30;
             }
          }
-         
+
          unsigned mult = machine->is_timex ? 2 : 1;
          uint16_t* pixel = image_buffer_2 + (y * hard_width + x + 1) * mult;
          unsigned i, j;
-         
+
          for (j = mult; j > 0; --j )
          {
             for (i = (width - 2) * mult; i > 0; --i)
@@ -760,12 +761,12 @@ static void render_video(void)
                *pixel = ~*pixel;
                pixel++;
             }
-            
+
             pixel += hard_width - (width - 2) * mult;
          }
-         
+
          pixel -= mult;
-         
+
          for (j = 22 * mult; j > 0; --j)
          {
             for (i = width * mult; i > 0; --i)
@@ -773,12 +774,12 @@ static void render_video(void)
                *pixel = ~*pixel;
                pixel++;
             }
-            
+
             pixel += hard_width - width * mult;
          }
-         
+
          pixel += mult;
-         
+
          for (j = mult; j > 0; --j)
          {
             for (i = (width - 2) * mult; i > 0; --i)
@@ -786,10 +787,10 @@ static void render_video(void)
                *pixel = ~*pixel;
                pixel++;
             }
-            
+
             pixel += hard_width - (width - 2) * mult;
          }
-         
+
          video_cb(image_buffer_2 + first_pixel, soft_width, soft_height, hard_width * sizeof(uint16_t));
       }
       else
@@ -802,50 +803,50 @@ static void render_video(void)
 void retro_run(void)
 {
    bool updated = false;
-   
+
    if (env_cb(RETRO_ENVIRONMENT_GET_VARIABLE_UPDATE, &updated) && updated)
    {
       int flags = update_variables(0);
-      
+
       if (flags & UPDATE_AV_INFO)
       {
          struct retro_system_av_info info;
          retro_get_system_av_info(&info);
          env_cb(RETRO_ENVIRONMENT_SET_SYSTEM_AV_INFO, &info);
       }
-      
+
       if (flags & UPDATE_GEOMETRY)
       {
          struct retro_game_geometry geometry;
-         
+
          // Here we use the "soft" resolution that is changed according to the
          // fuse_hide_border variable
          geometry.base_width = soft_width;
          geometry.base_height = soft_height;
-         
+
          geometry.max_width = MAX_WIDTH;
          geometry.max_height = MAX_HEIGHT;
          geometry.aspect_ratio = 0.0f;
-         
+
          env_cb(RETRO_ENVIRONMENT_SET_GEOMETRY, &geometry);
       }
-      
+
       if (flags & UPDATE_MACHINE)
       {
          machine_select( machine->id );
       }
    }
-   
+
    total_time_ms += frame_time;
    show_frame = some_audio = 0;
-   
+
    /*
    After playing Sabre Wulf's initial title music, fuse starts generating
    audio only for every other frame. RetroArch computes the FPS based on
    this and tries to call retro_run at double the rate to compensate for
    the audio. When vsync is on, FPS will cap at some value and the game
    will run slower.
-   
+
    This solution guarantees that every call to retro_run generates audio, so
    emulation runs steadly at 50 FPS. Ideally, we should investigate why fuse
    is doing that and fix it, but this solutions seems to work just fine.
@@ -856,7 +857,7 @@ void retro_run(void)
       event_do_events();
    }
    while (!some_audio);
-   
+
    render_video();
 }
 
@@ -885,9 +886,9 @@ void retro_set_controller_port_device(unsigned port, unsigned device)
          {
             settings_current.joystick_2_output = get_joystick(device);
          }
-         
+
          /* fallthrough */
-      
+
       default:
          input_devices[port] = device;
          break;
@@ -898,11 +899,11 @@ void retro_reset(void)
 {
    const char* ext;
    libspectrum_id_t type = indentify_file_get_ext(tape_data, tape_size, &ext);
-   
+
    char filename[32];
    snprintf(filename, sizeof(filename), "*%s", ext);
    filename[sizeof(filename) - 1] = 0;
-   
+
    fuse_emulation_pause();
    utils_open_file(filename, 1, &type);
    display_refresh_all();
@@ -920,14 +921,13 @@ size_t retro_serialize_size(void)
 bool retro_serialize(void *data, size_t size)
 {
    bool res = false;
-   
+
    if (size <= snapshot_size)
    {
       memcpy(data, snapshot_buffer, snapshot_size);
       res = true;
    }
 
-   free(snapshot_buffer);
    return res;
 }
 
@@ -957,6 +957,10 @@ bool retro_load_game_special(unsigned a, const struct retro_game_info *b, size_t
 
 void retro_unload_game(void)
 {
+   free(snapshot_buffer);
+   snapshot_buffer = NULL;
+   snapshot_size = 0;
+   
    free(tape_data);
 }
 
