6640ae1c834c31a776ad1d22c77ed919e8fe3c07
Vladimir Serbinenko <phcoder@google.com>
2020-05-06 02:02:59 +0200

    Emscripten fixes

--- fuse/ui/widget/widget.c
+++ fuse/ui/widget/widget.c
@@ -93,7 +93,7 @@ typedef struct widget_recurse_t {
 static widget_recurse_t widget_return[10]; /* The stack to recurse on */
 
 /* The settings used whilst playing with an options dialog box */
-settings_info widget_options_settings;
+extern settings_info widget_options_settings;
 
 static int widget_read_font( const char *filename )
 {
--- src/libretro.c
+++ src/libretro.c
@@ -476,7 +476,7 @@ static int get_joystick(unsigned device)
 #ifdef GIT_VERSION
 static char version[] = PACKAGE_VERSION " " GIT_VERSION;
 #else
-const char *fuse_githash;
+extern const char *fuse_githash;
 static char version[] = PACKAGE_VERSION " .......";
 #endif
 
